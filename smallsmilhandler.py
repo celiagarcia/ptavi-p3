bb2aee6c2af501f2590fd36ee4db4a79ae7668fd
celia garcia fernandez <cgarcia@iota20.aulas.gsyc.es>
2014-10-13 10:16:12 +0200

    PEP8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -5,20 +5,22 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
-    def __init__ (self):
-        
+    def __init__(self):
+
         self.lista = []
-        self.etiquetas = ['root-layout', 'region', 'img', 'audio', 'textstream']
+        self.etiquetas = [
+            'root-layout', 'region', 'img', 'audio', 'textstream']
         self.attributosD = {
             'root-layout': ['width', 'height'],
-            'region': ['id','top','left'],
-            'img': ['scr','region','begin','dur'],
-            'audio': ['src','begin','dur'],
+            'region': ['id', 'top', 'left'],
+            'img': ['scr', 'region', 'begin', 'dur'],
+            'audio': ['src', 'begin', 'dur'],
             'textstream': ['src', 'region']
             }
-      
+
     def startElement(self, name, attrs):
 
         diccionario = {}
@@ -28,10 +30,9 @@ class SmallSMILHandler(ContentHandler):
             for key in self.attributosD[name]:
                 diccionario[key] = attrs.get(key, "")
             self.lista.append(diccionario)
-            
+
     def get_tags(self):
         return self.lista
-     
 
 if __name__ == "__main__":
 
@@ -39,13 +40,5 @@ if __name__ == "__main__":
     small = SmallSMILHandler()
     parser.setContentHandler(small)
     parser.parse(open('karaoke.smil'))
-    
-    print small.get_tags()      
-        
-        
-        
-        
-        
-    
-        
-        
+
+    print small.get_tags()
