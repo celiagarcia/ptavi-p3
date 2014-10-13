539d33a473cad086cd91aa73d8cde8880d370c43
celia garcia fernandez <cgarcia@iota20.aulas.gsyc.es>
2014-10-13 10:27:18 +0200

    Devuelve las dos listas (orientado a objetos)

--- karaoke.py
+++ karaoke.py
@@ -10,49 +10,46 @@ import sys
 import os
 
 
+class KaraokeLocal():
+
+        def __init__ (self, fich):
+            parser = make_parser()
+            small = smallsmilhandler.SmallSMILHandler()
+            parser.setContentHandler(small)
+            parser.parse(fich)
+            self.lista = small.get_tags()
+        
+        def __str__(self):
+            salida = ""
+            for diccionario in self.lista:
+                salida += diccionario["name"] #salida = salida + diccionario["name"]
+               #print diccionario["name"],
+                for etiqueta in diccionario:
+                    if diccionario[etiqueta] and etiqueta != "name":
+                        salida += "\t" + etiqueta + '="' + diccionario[etiqueta] + '"'
+                        #print "\t" + etiqueta + '="' + diccionario[etiqueta] + '"',
+                salida += "\n"
+                #print
+            return salida            
+
+        def do_local(self):
+            for diccionario in self.lista:              
+                for etiqueta in diccionario:
+                    if diccionario[etiqueta].find("http://") == 0:
+                        recurso = diccionario[etiqueta]
+                        os.system("wget -q " + recurso)
+                        nuevo = diccionario[etiqueta].split('/')[-1]
+                        diccionario[etiqueta] = nuevo
+
 if __name__ == "__main__":
 
-    parser = make_parser()
-    small = smallsmilhandler.SmallSMILHandler()
-    parser.setContentHandler(small)
-    
     try:
         fich = open(sys.argv[1],'r')       
     except IOError:
 	    print "Usage: python karaoke.py file.smil"
 	    raise SystemExit
-	        
-    parser.parse(fich)
-    lista = small.get_tags()
-    
-    #imprimir lista                
-    for diccionario in lista:
-        print diccionario["name"],
-        for etiqueta in diccionario:
-            if diccionario[etiqueta] and etiqueta != "name":
-                 print "\t" + etiqueta + '="' + diccionario[etiqueta] + '"',
-        print            
-                         
-    #de romoto a local                
-    for diccionario in lista:              
-        for etiqueta in diccionario:
-            if diccionario[etiqueta].find("http://") == 0:
-                recurso = diccionario[etiqueta]
-                os.system("wget -q " + recurso)
-                nuevo = diccionario[etiqueta].split('/')[-1]
-                diccionario[etiqueta] = nuevo
-                
-    #imprimir lista                
-    for diccionario in lista:
-        print diccionario["name"],
-        for etiqueta in diccionario:
-            if diccionario[etiqueta] and etiqueta != "name":
-                 print "\t" + etiqueta + '="' + diccionario[etiqueta] + '"',
-        print            
-            
-            
-            
-            
-            
-            
-
+	    
+    karaoke = KaraokeLocal(fich)
+    print karaoke
+    karaoke.do_local()        
+    print karaoke
