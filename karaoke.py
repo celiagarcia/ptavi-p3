127325c4948e700d3d942b8205f0b3db51fc3644
celia garcia fernandez <cgarcia@iota20.aulas.gsyc.es>
2014-10-13 11:04:14 +0200

    PEP8

--- karaoke.py
+++ karaoke.py
@@ -12,28 +12,26 @@ import os
 
 class KaraokeLocal():
 
-        def __init__ (self, fich):
+        def __init__(self, fich):
             parser = make_parser()
             small = smallsmilhandler.SmallSMILHandler()
             parser.setContentHandler(small)
             parser.parse(fich)
             self.lista = small.get_tags()
-        
+
         def __str__(self):
             salida = ""
             for diccionario in self.lista:
-                salida += diccionario["name"] #salida = salida + diccionario["name"]
-               #print diccionario["name"],
+                salida += diccionario["name"]
                 for etiqueta in diccionario:
                     if diccionario[etiqueta] and etiqueta != "name":
-                        salida += "\t" + etiqueta + '="' + diccionario[etiqueta] + '"'
-                        #print "\t" + etiqueta + '="' + diccionario[etiqueta] + '"',
+                        salida += "\t" + etiqueta + '="'
+                        salida += diccionario[etiqueta] + '"'
                 salida += "\n"
-                #print
-            return salida            
+            return salida
 
         def do_local(self):
-            for diccionario in self.lista:              
+            for diccionario in self.lista:
                 for etiqueta in diccionario:
                     if diccionario[etiqueta].find("http://") == 0:
                         recurso = diccionario[etiqueta]
@@ -44,12 +42,13 @@ class KaraokeLocal():
 if __name__ == "__main__":
 
     try:
-        fich = open(sys.argv[1],'r')       
+        fich = open(sys.argv[1], 'r')
     except IOError:
-	    print "Usage: python karaoke.py file.smil"
-	    raise SystemExit
-	    
+        sys.exit("Usage: python karaoke.py file.smil")
+    except IndexError:
+        sys.exit("Usage: python karaoke.py file.smil")
+
     karaoke = KaraokeLocal(fich)
     print karaoke
-    karaoke.do_local()        
+    karaoke.do_local()
     print karaoke
